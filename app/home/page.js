6c342faff268709a4c1e5a957fc9911e348b5939
Gyane-git <alpen850@gmail.com>
2026-04-27 16:34:28 +0545

    title change

---
+++ app/home/page.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import HeroSection from "@/components/Herosection"
+import ImageSlider from "@/components/Imageslider"
+import ProductSection from "@/components/Productsection"
+import WhyChooseNityagro from "@/components/Whychoosenityagro"
+import Customerreviews from "@/components/Customerreviews"
+import Faq from "@/components/Faq"
+import Shopctabanner from "@/components/Shopctabanner"
+
+function home() {
+  return (
+    <div>
+      <HeroSection/>
+      <ImageSlider/>
+      <ProductSection/>
+      <WhyChooseNityagro/>
+      <Customerreviews />
+      <Faq/>
+      <Shopctabanner/>
+    </div>
+  )
+}
+
+export default home
