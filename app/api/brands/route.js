7b8351f42808c4c60863967c3f3e977afcfe3e6c
scilentknight <scilentknight512@gmail.com>
2026-05-01 13:25:33 +0545

    add-product page ui added

--- app/api/brands/route.js
+++ app/api/brands/route.js
@@ -1,12 +1,12 @@
-export async function GET() {
-  return Response.json({
-    success: true,
-    data: {
-      brands: [
-        { id: 1, brand: "Brand 1" },
-        { id: 2, brand: "Brand 2" },
-        { id: 3, brand: "Brand 3" },
-      ],
-    },
-  });
-}
+// export async function GET() {
+//   return Response.json({
+//     success: true,
+//     data: {
+//       brands: [
+//         { id: 1, brand: "Brand 1" },
+//         { id: 2, brand: "Brand 2" },
+//         { id: 3, brand: "Brand 3" },
+//       ],
+//     },
+//   });
+// }
--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -1,12 +1,12 @@
-export async function GET() {
-  return Response.json({
-    success: true,
-    data: {
-      categories: [
-        { id: 1, category: "Category 1" },
-        { id: 2, category: "Category 2" },
-        { id: 3, category: "Category 3" },
-      ],
-    },
-  });
-}
+// export async function GET() {
+//   return Response.json({
+//     success: true,
+//     data: {
+//       categories: [
+//         { id: 1, category: "Category 1" },
+//         { id: 2, category: "Category 2" },
+//         { id: 3, category: "Category 3" },
+//       ],
+//     },
+//   });
+// }
