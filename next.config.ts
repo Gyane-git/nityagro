8f4281504e7436e5ffa0e6f114596dafd9f111e1
Gyanendra Shah <alpen850@gmail.com>
2026-05-15 11:27:00 +0545

    fix: live api env + categories no-cache + helper stability

--- next.config.ts
+++ next.config.ts
@@ -1,56 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: [
-    "dentalnepal.com",
-    "www.dentalnepal.com",
-    "gargdental.vercel.app",
+    "nityagro.globaltech.com.np",
     "localhost:3000",
-    
   ],
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/index.php",
-  //       destination: "/dashboard",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/index.plx",
-  //       destination: "/dashboard",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+
   images: {
     remotePatterns: [
       {
-        protocol: "https", //for server host for local change to http
-        hostname: "gargdemo.omsok.com", //for server host for local change api url
-        pathname: "/**",
-      },
-      {
         protocol: "https",
-        hostname: "garg.omsok.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "gargdental.omsok.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dentalnepal.com",
+        hostname: "nityagro.globaltech.com.np",
         pathname: "/**",
       },
       {
         protocol: "http",
-        hostname: "dentalnepal.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "gargdental.vercel.app",
+        hostname: "nityagro.globaltech.com.np",
         pathname: "/**",
       },
     ],
