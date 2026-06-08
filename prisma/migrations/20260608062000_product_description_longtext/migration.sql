03943964ce0f50b2a296ce76bad85928baaba730
Gyane-git <alpen850@gmail.com>
2026-06-08 12:26:59 +0545

    gggg

---
+++ prisma/migrations/20260608062000_product_description_longtext/migration.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `products`
+  MODIFY `productDescription` LONGTEXT NULL,
+  MODIFY `nutritionInfo` LONGTEXT NULL,
+  MODIFY `cookingInstruction` LONGTEXT NULL,
+  MODIFY `storageInstruction` LONGTEXT NULL;
